18fc5786c084a45728197560e1dc4b63962fae5e
Cere <c_cere2909@hotmail.com>
2025-04-03 11:49:01 +0000

    terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,21 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from './Navbar.jsx';
+import { Jumbotron } from "./Jumbotron.jsx";
+import {MyCard} from "./MyCard.jsx";
 
 //create your first component
-const Home = () => {
+export const Home = () => {
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+            <Navbar/>
+			<div className="container">
+			<Jumbotron/>
+			<div className="row">
+			    <MyCard  imgurl={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wallpapersafari.com%2F30%2F62%2FjHBzTk.jpg&f=1&nofb=1&ipt=62476ded9fc263b75ff44b8d025d575f446e15c270ad1403322a60311c94ca4a&ipo=images'}/>
+				<MyCard  imgurl={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wallpapersafari.com%2F30%2F62%2FjHBzTk.jpg&f=1&nofb=1&ipt=62476ded9fc263b75ff44b8d025d575f446e15c270ad1403322a60311c94ca4a&ipo=images'}/>
+				<MyCard  imgurl={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wallpapersafari.com%2F30%2F62%2FjHBzTk.jpg&f=1&nofb=1&ipt=62476ded9fc263b75ff44b8d025d575f446e15c270ad1403322a60311c94ca4a&ipo=images'}/>
+			</div>
+		    </div>
 		</div>
 	);
 };
-
-export default Home;
